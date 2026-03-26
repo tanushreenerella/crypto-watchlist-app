84626460f0725e5aed0d3a5b0e61fe86005a72b3
Tanushree Nerella <tanushreenerella697@gmail.com>
2026-03-27 01:45:51 +0530

    switched to pbkdf2 to fix bcrypt issue

--- crypto-backend/app/utils/hash.py
+++ crypto-backend/app/utils/hash.py
@@ -1,15 +1,12 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def clean_password(p: str):
-    return str(p).strip()[:72]   # 🔥 normalize EVERYTHING
-
+pwd_context = CryptContext(
+    schemes=["pbkdf2_sha256"],   # 🔥 CHANGE HERE
+    deprecated="auto"
+)
 
 def hash_password(password: str):
-    return pwd_context.hash(clean_password(password))
-
+    return pwd_context.hash(password)
 
 def verify_password(plain, hashed):
-    return pwd_context.verify(clean_password(plain), hashed)
+    return pwd_context.verify(plain, hashed)
