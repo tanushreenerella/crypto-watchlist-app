6e197c688183fb3f491b6129b78a902700505274
Tanushree Nerella <tanushreenerella697@gmail.com>
2026-03-27 00:36:05 +0530

    deployment fixes

--- crypto-backend/app/main.py
+++ crypto-backend/app/main.py
@@ -19,14 +19,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 @app.get("/")
 def root():
     return {"message": "API is running 🚀"}
