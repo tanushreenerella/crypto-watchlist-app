7348f1cdc9a5865302c13aa9ac400bd3a43ee42d
Tanushree Nerella <tanushreenerella697@gmail.com>
2026-03-27 01:26:26 +0530

    password hashing issue

--- crypto-backend/app/utils/hash.py
+++ crypto-backend/app/utils/hash.py
@@ -4,8 +4,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
-    return pwd_context.hash(password[:72])   # ✅ FIX bcrypt issue
+    return pwd_context.hash(password[:72])   # ✅ truncate
 
 
 def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+    return pwd_context.verify(plain[:72], hashed)  # ✅ FIX HERE
